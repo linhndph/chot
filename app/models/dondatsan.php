8f7efbc144ffdaaa3fb82fca07b541f167048b07
linhndph <linhndph33107@fpt.edu.vn>
2023-12-04 19:55:17 +0700

    update

---
+++ app/models/dondatsan.php
@@ -0,0 +1,37 @@
+<?php 
+function insert_hoa_don($ma_dat_san, $tong_don_hang, $id_tk,$ten_kh,$sdt_kh ,$email,$dia_chi_kh,$ten_san,$ca_san,$trang_thaihd,$ngay_su_dung){
+    $sql = "INSERT INTO `chi_tiet_dat_san`(`ma_dat_san`, `tong_don_hang`,`id_tai_khoan` ,`ten_kh`, `sdt_kh`,`email`,`dia_chi_kh`,`ten_san`,`ca_san`,`trang_thaihd`,`ngay_su_dung`) 
+    VALUES ('$ma_dat_san', '$tong_don_hang','$id_tk' ,'$ten_kh', '$sdt_kh','$email','$dia_chi_kh','$ten_san','$ca_san','$trang_thaihd','$ngay_su_dung');";
+    pdo_execute($sql);
+    
+}
+function load_billdatsan(){
+    $sql="select * from chi_tiet_dat_san join tb_tai_khoan on chi_tiet_dat_san.id_tai_khoan= tb_tai_khoan.id_tai_khoan";
+    $bill= pdo_query($sql);
+    return $bill;
+
+}
+function loadone_billdatsan($id_tai_khoan){
+    $sql= "select * from chi_tiet_dat_san join tb_tai_khoan on chi_tiet_dat_san.id_tai_khoan= tb_tai_khoan.id_tai_khoan where chi_tiet_dat_san.id_tai_khoan='$id_tai_khoan'";
+    $billsan= pdo_query($sql);
+    return $billsan;
+
+}
+
+function loadone_billdatsan1($id_tai_khoan){
+    $sql= "select * from chi_tiet_dat_san join tb_tai_khoan on chi_tiet_dat_san.id_tai_khoan= tb_tai_khoan.id_tai_khoan where chi_tiet_dat_san.id_tai_khoan='$id_tai_khoan' order by id_ctdatsan desc limit 1 ";
+    $billsan= pdo_query($sql);
+    return $billsan;
+
+}
+function Load_Oneev($id, $sp){
+    $one = $sp;
+    $sql = "SELECT * FROM $one WHERE id_ctdatsan = '$id'";
+    return  pdo_query_one($sql);
+    
+}
+function delete_datsan($id_ctdatsan){
+    $sql="DELETE FROM chi_tiet_dat_san where id_ctdatsan= $id_ctdatsan";
+    pdo_execute($sql);
+}
+?>
